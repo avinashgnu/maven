858b629243a7187d7bd84ca6465036eec5c7cd10
avinashgnu <avinash.mamidisetti9@gmail.com>
2016-06-09 10:37:41 +0530

    kfgherifjkjwrenflkqej
    nmdbhjwhedjklqdkq,md

---
+++ FirstDemo/src/Project/JunitFirstProgram.java
@@ -0,0 +1,14 @@
+package Project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitFirstProgram {
+
+	@Test
+	public void test() {
+	//	fail("Not yet implemented");
+	}
+
+}
